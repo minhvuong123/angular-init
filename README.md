c5b66d027ef0aa4e459ad90e4cea8d0360a14604
minhvuong123 <minhvuong12222@gmail.com>
2024-03-23 21:00:13 +0700

    deploy angular

--- README.md
+++ README.md
@@ -25,3 +25,8 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+static web hosting
+firebase hosting
+
+https://academind.com/tutorials/angular-q-a#how-to-fix-broken-routes-after-deployment
--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,15 +1,25 @@
 
 import { NgModule } from '@angular/core';
 import { SharedRoutingModule } from './shared-routing.module';
+import { CommonModule } from '@angular/common';
+import { LoggingService } from './services/logging.service';
 
 @NgModule({
   declarations: [
-  
+    // import component herr
   ],
   imports: [
+    CommonModule,
     SharedRoutingModule
   ],
-  exports: []
+  providers: [
+    // LoggingService
+  ],
+  exports: [
+    CommonModule,
+    // LoggingService // import in here will separate when using lazy loading
+    // export component here
+  ]
 })
 export class SharedModule { }
 // import to other modules, this module includes components use for al project
